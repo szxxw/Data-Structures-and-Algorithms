97c3ff9dfaf36751f330977a30d57e8ee2b98041
莘莘 <2102462967@qq.com>
2024-01-01 22:00:54 +0800

    update files

--- src/二叉树层序遍历.java
+++ src/二叉树层序遍历.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class 二叉树层序遍历 {
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode new_root = q.poll();
+                level.add(new_root.val);
+                if(new_root.left != null) q.offer(new_root.left);
+                if(new_root.right != null) q.offer(new_root.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
 }
